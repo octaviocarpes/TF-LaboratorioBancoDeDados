49ab1e1c0840a7482826f4d95c65fa9831cb7374
Angelo Calebe Araujo da Rocha <calebe@procempa.com.br>
2017-11-30 17:40:48 -0200

    Mais consultas

---
+++ modelo_bd/consultas/Alunos_Semestre.sql
@@ -0,0 +1,9 @@
+SELECT m.numero, a.nome, a.genero, a.data_nasc, a.email, a.endereco, c.nome, e.nome, cs.nome
+FROM Aluno a
+INNER JOIN Matricula m ON a.Matricula_numero = m.numero
+INNER JOIN Cidade c ON a.Cidade_id = c.id
+INNER JOIN Estado e ON c.Estado_uf = e.uf
+INNER JOIN Curso cs ON a.Curso_id = cs.id
+INNER JOIN Semestre s ON a.Matricula_numero = s.Aluno_Matricula_numero
+WHERE s.periodo = periodo
+ORDER BY a.nome;
---
+++ modelo_bd/consultas/Formados.sql
@@ -0,0 +1,22 @@
+SELECT m.numero, a.nome, a.genero, a.data_nasc, a.email, a.endereco, c.nome, e.nome, cs.nome, s.periodo as Ultimo_Semestre
+FROM Aluno a
+INNER JOIN Matricula m ON a.Matricula_numero = m.numero
+INNER JOIN Cidade c ON a.Cidade_id = c.id
+INNER JOIN Estado e ON c.Estado_uf = e.uf
+INNER JOIN Curso cs ON a.Curso_id = cs.id
+INNER JOIN Semestre s ON a.Matricula_numero = s.Aluno_Matricula_numero
+INNER JOIN Semestre sd ON s.id = sd.Semestre_id
+INNER JOIN Disciplina d ON sd.Disciplina_id = d.id
+WHERE s.atual = 0 AND NOT EXISTS (
+	SELECT d1.id
+	FROM Disciplina d1
+	INNER JOIN Curriculo_Disciplina cd1 ON d1.id = cd1.Disciplina_id
+	INNER JOIN Curriculo c1 ON cd1.Curriculo_id = c1.id
+	MINUS
+	SELECT d2.id
+	FROM Disciplina d2
+	INNER JOIN Semestre_Disciplina sd2 ON d2.id = sd2.Semestre_id
+	INNER JOIN Semestre s2 ON sd2.Semestre_id = s2.id
+	WHERE s2.Aluno_Matricula_numero = a.Matricula_Numero
+)
+ORDER BY a.nome;

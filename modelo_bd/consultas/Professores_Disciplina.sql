c635f9da7090339331e809199b4000435ecab5f2
angelo.rocha <angelo.calebe@acad.pucrs.br>
2017-12-01 16:21:41 -0200

    Mais consultas

--- modelo_bd/consultas/Professores_Disciplina.sql
+++ modelo_bd/consultas/Professores_Disciplina.sql
@@ -1,4 +1,4 @@
-SELECT m.numero as matricula, p.nome, p.genero, p.data_nasc, p.email, p.endereco, c.nome as cidade, e.nome as estado, d.nome as disciplina
+SELECT m.numero as matricula, p.nome as professor, p.genero, p.data_nasc, p.email, p.endereco, c.nome as cidade, e.nome as estado, d.nome as disciplina
 FROM Professor p
 INNER JOIN Matricula m ON p.Matricula_numero = m.numero
 INNER JOIN Cidade c ON p.Cidade_id = c.id
